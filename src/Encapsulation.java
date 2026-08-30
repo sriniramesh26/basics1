71b4e038e44fdff4a59880ba6055ace50a5c866d
SRINIVASAN R <sriniramesh26@gmail.com>
2026-08-30 23:19:02 +0530

    Add encapsulation code

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+class BankAccount{
+    private double balance;                 //private field - hidden from outside
+    public BankAccount(double balance){
+        if(balance >= 0){
+            this.balance = balance;
+        }
+    }
+
+    // Getter - controlled read access
+    public double getBalance(){
+        return balance;
+    }
+
+    // Setter with valuatioon - controlled with access
+public void deposit(double amount){
+    if(amount >0){
+        balance += amount;
+    }
+}
+
+public void withdraw(double amount){
+    if(amount <0 && amount <= balance)   balance -= amount;
+    else System.out.println("Invalid withdrawal amount");
+    }
+}
+
 
 public class Encapsulation {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(10); //create an object of BankAccount class
+        account.deposit(40);   //calling a deposit method
+        account.withdraw(20);  //calling a withdraw method
+        System.out.println("Balance is: " + account.getBalance());
+
+
+    }
+
 }
